8cb9b9c8c6fa454f8aae4dfa40f744168d3bc0fc
Sandhiya10 <Sandhiya2009@gmail.com>
2017-06-10 23:05:30 +0530

    Create LargeNum.java

--- Practice Programs/LargeNum.java
+++ Practice Programs/LargeNum.java
@@ -2,17 +2,13 @@ public class LargeNum
 {
 public static void main(String[] s)
 {
-int largenumber;
-int arr[]={120,567,203};
-largenumber=arr[0];
-for(int i=0;i<arr.length-1;i++)
-{
-if(largenumber<arr[i])
-largenumber=arr[i];
-
-}
+  int largenumber;
+  int arr[]={120,567,203};
+  largenumber=arr[0];
+    for(int i=0;i<arr.length-1;i++)
+      {
+        if(largenumber<arr[i])
+        largenumber=arr[i];
+      }
 System.out.println("Largest Number is "+largenumber);
-}
-
-
-}
+}}
